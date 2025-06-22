2e8c12aca5a06da3196e4f6bc5fae1e883e50787
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-06-22 10:15:32 +0330

    js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const boxes=document.querySelectorAll(".box"),
-image=document.querySelectorAll(".image");
+const boxes = document.querySelectorAll(".box"),
+    image = document.querySelector(".image");
 
-console.log(boxes,image);
+boxes.forEach(box => {
+    box.addEventListener("dragover", e => {
+        e.preventDefault();
+        box.classList.add("hovered");
+    });
+    box.addEventListener("dragleave", () => {
+        box.classList.remove("hovered");
+    });
+    box.addEventListener("drop", () => {
+        box.appendChild(image);
+        box.classList.remove("hovered");
+    })
+})
